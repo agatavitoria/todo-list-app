3cb78dae87f10e8d39376c370352bb4e49c61878
agatavitoria <agatavitoriadelima@gmail.com>
2023-03-07 18:06:02 -0300

    feat: dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.red),
+      theme: ThemeData.dark(),
       home: const HomeView(),
     );
   }
